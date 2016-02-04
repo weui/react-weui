454dec2d78d7eaa098dbde6ccf005a608696dba2
n7best <n7best@gmail.com>
2016-02-03 22:47:53 -0500

    add grids and relate doc & test, update package.json to compatible with 0.3.0

---
+++ src/components/grid/grids.js
@@ -0,0 +1,44 @@
+/**
+ * Created by n7best
+ */
+
+
+
+import React from 'react';
+import classNames from 'classnames';
+import Grid from './grid';
+
+export default class Grids extends React.Component {
+    static propTypes = {
+      data: React.PropTypes.array
+    };
+
+    static defaultProps = {
+      data: []
+    };
+
+    renderData(data) {
+        return data.map((item,i) => {
+            return <Grid
+                    key={i}
+                    icon={item.icon}
+                    label={item.label}
+                    {...item}
+                    />;
+        });
+    }
+
+    render() {
+
+        const {children, data, className, ...others} = this.props;
+        const cls = classNames({
+            weui_grids: true
+        }, className);
+
+        return (
+            <div className={cls} {...others}>
+            {data.length > 0 ? this.renderData(data) : children}
+            </div>
+        );
+    }
+};
--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ import Dialog from './components/dialog/index';
 import Msg from './components/msg/index';
 import Article from './components/article/index';
 import Icon from './components/icon/index';
+import {Grids, Grid, GridIcon, GridLabel} from './components/grid/index';
 
 export default {
     Button,
@@ -37,5 +38,9 @@ export default {
     Dialog,
     Msg,
     Article,
-    Icon
+    Icon,
+    Grids,
+    Grid,
+    GridIcon,
+    GridLabel
 };
