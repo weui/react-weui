045421a1e0451ff6a5d4ed9068cfaa4e2a034af8
n7best <n7best@gmail.com>
2016-02-16 11:10:56 -0500

    add textarea w/o test,doc

---
+++ src/components/form/textarea.js
@@ -0,0 +1,44 @@
+/**
+ * Created by n7best
+ */
+
+
+
+import React, { Component, PropTypes } from 'react';
+import CellBody from '../cell/cell_body';
+import classNames from 'classnames';
+
+export default class TextArea extends React.Component {
+    static propTypes = {
+        showCounter: PropTypes.bool,
+    };
+
+    static defaultProps = {
+        showCounter: true,
+    };
+
+    state = {
+        textCounter: 0
+    };
+
+    handleChange(e){
+      this.setState({textCounter: e.target.value.length});
+      //forward event to props if any
+      if(this.props.onChange) this.props.onChange(e);
+    }
+
+    render() {
+        const { className, children, showCounter, maxlength, ...others } = this.props;
+        const cls = classNames({
+            weui_textarea: true,
+            [className]: className
+        });
+
+        return (
+          <CellBody>
+            <textarea className={cls} maxLength={maxlength} onChange={this.handleChange.bind(this)} {...others}>{children}</textarea>
+            {showCounter ? <div className="weui_textarea_counter"><span>{this.state.textCounter}</span>/{maxlength ? maxlength : '∞'}</div> : false}
+          </CellBody>
+        );
+    }
+};
