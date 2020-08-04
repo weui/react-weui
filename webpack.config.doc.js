a7bb788e01a97ab66ea1656f11110e12ff72a3db
z2014 <591763389@qq..com>
2020-08-04 23:07:58 +0800

    feat:delete comment

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -116,26 +116,6 @@ if (!isProduction) {
   plugins.push(
     new webpack.HotModuleReplacementPlugin()
     );
-} else {
-  //   plugins.push(
-  //       new webpack.optimize.UglifyJsPlugin({
-  //         compress: {
-  //           warnings: false,
-  //           screw_ie8: true,
-  //           conditionals: true,
-  //           unused: true,
-  //           comparisons: true,
-  //           sequences: true,
-  //           dead_code: true,
-  //           evaluate: true,
-  //           if_return: true,
-  //           join_vars: true,
-  //         },
-  //         output: {
-  //           comments: false,
-  //         },
-  //       })
-  // );
 }
 
 module.exports = {
